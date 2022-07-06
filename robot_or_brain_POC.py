c5b2493f504581d86d54ff4d1c063f67a82ea64b
Christiaan Meijer <c.meijer@esciencecenter.nl>
2022-07-06 11:28:13 +0200

    add training procedure to script

--- robot_or_brain_POC.py
+++ robot_or_brain_POC.py
@@ -6,8 +6,8 @@ wandb.init(project="robot-or-brain-POC", entity="robot-or-brain")
 
 config = {
   "learning_rate": 0.001,
-  "epochs": 100,
-  "batch_size": 128
+  "epochs": 200,
+  "batch_size": 32,
 }
 
 wandb.config = config
@@ -19,6 +19,7 @@ from tensorflow.keras.applications.resnet_v2 import preprocess_input, decode_pre
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
 from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.utils import image_dataset_from_directory
 import numpy as np
 
 
@@ -43,24 +44,70 @@ model = Model(inputs=base_model.input, outputs=predictions)
 # Training only top layers i.e. the layers which we have added in the end
 for layer in base_model.layers:
     layer.trainable = False
-    
-    
-model.compile(optimizer=Adam(lr=config['learning_rate']), loss='categorical_crossentropy', metrics = ['accuracy'])
+
+
+model.compile(optimizer=Adam(lr=config['learning_rate']), loss='sparse_categorical_crossentropy', metrics = ['accuracy'])
 
 
 # The following line cannot be run from a python script.
 #!wget "https://mtintegraal.nl/media/articles/926/ai-technische-onmogelijkheden.jpg"
 
 
-img_path = 'ai-technische-onmogelijkheden.jpg'
-img = image.load_img(img_path, target_size=(224, 224))
-x = image.img_to_array(img)
-x = np.expand_dims(x, axis=0)
-x = preprocess_input(x)
+#img_path = 'ai-technische-onmogelijkheden.jpg'
+#img = image.load_img(img_path, target_size=(224, 224))
+#x = image.img_to_array(img)
+#x = np.expand_dims(x, axis=0)
+#x = preprocess_input(x)
 
-print(x.shape)
-img
+#print(x.shape)
+#img
 
 
-preds = model.predict(x)
-preds.shape
+#preds = model.predict(x)
+#preds.shape
+
+
+# ----
+# Let's train the model now
+# ----
+
+train_ds = image_dataset_from_directory(
+    '../robot_or_brain_data/images_by_class/train',
+    labels="inferred",
+    label_mode="int",
+    class_names=None,
+    color_mode="rgb",
+    batch_size=config['batch_size'],
+    image_size=(224, 224),
+    shuffle=True,
+    seed=0,
+    validation_split=0.2,
+    subset='training',
+    interpolation="bilinear",
+    follow_links=False,
+    crop_to_aspect_ratio=False,
+)
+
+validation_ds = image_dataset_from_directory(
+    '../robot_or_brain_data/images_by_class/train',
+    labels="inferred",
+    label_mode="int",
+    class_names=None,
+    color_mode="rgb",
+    batch_size=config['batch_size'],
+    image_size=(224, 224),
+    shuffle=True,
+    seed=0,
+    validation_split=0.2,
+    subset='validation',
+    interpolation="bilinear",
+    follow_links=False,
+    crop_to_aspect_ratio=False,
+)
+
+model.fit(
+    train_ds,
+    epochs=config['epochs'],
+    validation_data=validation_ds,
+    callbacks=[WandbCallback()],
+)
