3b530771de5b6f7b1eaa4a8e716338e4a39a13a7
Christiaan Meijer <c.meijer@esciencecenter.nl>
2022-09-13 11:50:08 +0200

    clean train script

--- robot_or_brain_POC.py
+++ robot_or_brain_POC.py
@@ -12,13 +12,10 @@ config = {
 wandb.config = config
 
 from tensorflow.keras.applications.resnet_v2 import ResNet50V2
-from tensorflow.keras.preprocessing import image
-from tensorflow.keras.applications.resnet_v2 import preprocess_input, decode_predictions
 from tensorflow.keras.models import Model
-from tensorflow.keras.layers import Dense, GlobalAveragePooling2D
+from tensorflow.keras.layers import Dense
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.utils import image_dataset_from_directory
-import numpy as np
 
 base_model = ResNet50V2(
     include_top=False,
@@ -40,23 +37,6 @@ for layer in base_model.layers:
     layer.trainable = False
 
 model.compile(optimizer=Adam(lr=config['learning_rate']), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-
-# The following line cannot be run from a python script.
-# !wget "https://mtintegraal.nl/media/articles/926/ai-technische-onmogelijkheden.jpg"
-
-
-# img_path = 'ai-technische-onmogelijkheden.jpg'
-# img = image.load_img(img_path, target_size=(224, 224))
-# x = image.img_to_array(img)
-# x = np.expand_dims(x, axis=0)
-# x = preprocess_input(x)
-
-# print(x.shape)
-# img
-
-
-# preds = model.predict(x)
-# preds.shape
 
 
 # ----
