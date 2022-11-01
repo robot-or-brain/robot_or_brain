68ee5d723e8466f317b92232401af0b42c1c63b8
Christiaan Meijer <c.meijer@esciencecenter.nl>
2022-11-01 10:37:17 +0100

    add saving of resnet50 features

--- clip_save_features.py
+++ clip_save_features.py
@@ -1,6 +1,13 @@
 import argparse
 from pathlib import Path
 
+import clip
+import numpy as np
+import tensorflow as tf
+import torch
+from PIL import Image
+from tensorflow.keras.applications.resnet_v2 import ResNet50V2, preprocess_input
+from tensorflow.keras.models import Model
 from tqdm import tqdm
 
 from utils import load_dataset
@@ -9,10 +16,6 @@ parser = argparse.ArgumentParser(description='Train classifier on directory stru
 parser.add_argument('--data_base_path', type=Path, help='Path to dir containing the metadata csv file.', required=True)
 
 args = parser.parse_args()
-
-from PIL import Image
-import clip
-import torch
 
 
 def predict_image_with_clip(image_path):
@@ -25,14 +28,34 @@ def predict_image_with_clip(image_path):
 
     with torch.no_grad():
         logits_per_image = model.encode_image(image)
-        probs = logits_per_image.softmax(dim=-1).cpu().numpy()
-    return probs
+        # probs = logits_per_image.softmax(dim=-1).cpu().numpy()
+    return logits_per_image.cpu().numpy()
+
+
+def predict_image_with_resnet(image_path):
+    base_model = ResNet50V2(
+        include_top=False,
+        weights='imagenet',
+        input_tensor=None,
+        input_shape=None,
+        pooling='avg'
+    )
+
+    input_layer = tf.keras.Input(shape=(None, None, 3))
+    preprocessed_input = preprocess_input(input_layer)
+    prediction_layer = base_model(preprocessed_input)
+    model = Model(inputs=input_layer, outputs=prediction_layer)
+
+    img = Image.open(image_path)
+    np_array = np.expand_dims(np.array(img), axis=0)
+
+    return model.predict(np_array)
 
 
 def encode_and_save(base_dir, split):
     data_set, _ = load_dataset(split, base_dir=base_dir / 'images_by_class')
-    encodings = [predict_image_with_clip(p) for p in tqdm(data_set['paths'])]
-    data_set['clip_features'] = encodings
+    data_set['clip_features'] = [predict_image_with_clip(p) for p in tqdm(data_set['paths'])]
+    data_set['resnet_features'] = [predict_image_with_resnet(p) for p in tqdm(data_set['paths'])]
     data_set.to_pickle(base_dir / f'{split}.pk')
 
 
